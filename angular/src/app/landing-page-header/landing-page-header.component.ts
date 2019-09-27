1c8ff9ed76935ced5fa3652f34d148c9200c6899
omki <aagaardmikkeli@gmail.com>
2019-09-27 03:39:28 +0200

    redirect to events from landing when logged in

--- angular/src/app/landing-page-header/landing-page-header.component.ts
+++ angular/src/app/landing-page-header/landing-page-header.component.ts
@@ -10,98 +10,97 @@ import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'landing-page-header',
-    templateUrl: './landing-page-header.component.html',
-    styleUrls: ['./landing-page-header.component.css']
+  selector: 'landing-page-header',
+  templateUrl: './landing-page-header.component.html',
+  styleUrls: ['./landing-page-header.component.css']
 })
 export class LandingPageHeaderComponent implements OnInit {
 
-    isMobile = false;
-    hide: true;
-    isLoggedOn = false;
-    username: string;
-    singUpVisible: boolean = true;
+  isMobile = false;
+  hide: true;
+  isLoggedOut = false;
+  username: string;
+  singUpVisible: boolean = true;
 
-    public loginForm = new FormGroup({
-        email: new FormControl(''),
-        password: new FormControl('')
+  public loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor(
+    private authService: AuthService,
+    public dialog: MatDialog,
+    private mds: MobileDetectorService,
+    private spinner: NgxSpinnerService,
+    private ufbs: UserFirebaseService,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+
+    this.singUpVisible = this.router.url !== '/sign-up';
+
+    this.isMobile = this.mds.check();
+    this.authService.afAuth.auth.onAuthStateChanged(user => {
+      if (user !== null && user.emailVerified) {
+        this.router.navigate(['/events']);
+      } else {
+        this.isLoggedOut = true;
+      }
+      //   else if (user && !user.emailVerified) {
+      //     this.toastr.warning("Det lader til at du har et login, men ikke har bekræftet din email. Dette skal gøres inden 24 timer.", 'ℹ️ ')
+      //   }
+    });
+  }
+
+  tryLogin(formData) {
+    this.spinner.show();
+    this.authService.doLogin(formData).then(() => {
+      this.spinner.hide();
+    }).catch(() => {
+      this.spinner.hide();
+    });
+  }
+
+  tryFacebookLogin() {
+    this.spinner.show();
+    this.authService.doFacebookLogin().then((callback) => {
+      this.spinner.hide();
+    }).catch((error) => {
+      this.spinner.hide();
+    });
+  }
+
+  tryGoogleLogin() {
+    this.spinner.show();
+    this.authService.doGoogleLogin().then(() => {
+      this.spinner.hide();
+    }).catch(() => {
+      this.spinner.hide();
+    });
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(MobileLoginHeaderComponent, {
+      width: '250px',
     });
 
-    constructor(
-        private authService: AuthService,
-        public dialog: MatDialog,
-        private mds: MobileDetectorService, 
-        private spinner: NgxSpinnerService,
-        private ufbs: UserFirebaseService, 
-        private toastr: ToastrService,
-        private router: Router
-        ) { }
+    dialogRef.afterClosed().subscribe(result => {
 
-    ngOnInit() {
-        
-        this.singUpVisible = this.router.url !== '/sign-up';
+    });
+  }
 
-        this.isMobile = this.mds.check();
-        this.authService.afAuth.auth.onAuthStateChanged(user => {
-            if (user !== null && user.emailVerified) {
-                this.isLoggedOn = true;
-                if (this.router.url === '/landing-page') {
-                    this.router.navigate(['/events']);
-                }
-            }
-            //   else if (user && !user.emailVerified) {
-            //     this.toastr.warning("Det lader til at du har et login, men ikke har bekræftet din email. Dette skal gøres inden 24 timer.", 'ℹ️ ')
-            //   }
-        });
+  showSuccess(msg = "", status = "") {
+    this.toastr.clear();
+    this.toastr.success(msg, status);
+  }
+
+  translateCallback(message: string) {
+    if (message.includes('An account already exists')) {
+      return "En konto findes allerede oprettet med den samme email addresse. Dette skyldes at du allerede har en Google konto eller Facebook konto tilknyttet SingleNetværket. Log derfor ind med den anden.";
     }
-
-    tryLogin(formData) {
-        this.spinner.show();
-        this.authService.doLogin(formData).then(() => {
-            this.spinner.hide();
-        }).catch(() => {
-            this.spinner.hide();
-        });
-    }
-
-    tryFacebookLogin() {
-        this.spinner.show();
-        this.authService.doFacebookLogin().then((callback) => {
-            this.spinner.hide();
-        }).catch((error) => {
-            this.spinner.hide();
-        });
-    }
-
-    tryGoogleLogin() {
-        this.spinner.show();
-        this.authService.doGoogleLogin().then(() => {
-            this.spinner.hide();
-        }).catch(() => {
-            this.spinner.hide();
-        });
-    }
-
-    openDialog(): void {
-        const dialogRef = this.dialog.open(MobileLoginHeaderComponent, {
-            width: '250px',
-        });
-
-        dialogRef.afterClosed().subscribe(result => {
-
-        });
-    }
-
-    showSuccess(msg = "", status = "") {
-        this.toastr.clear();
-        this.toastr.success(msg, status);
-    }
-
-    translateCallback(message: string) {
-        if (message.includes('An account already exists')) {
-            return "En konto findes allerede oprettet med den samme email addresse. Dette skyldes at du allerede har en Google konto eller Facebook konto tilknyttet SingleNetværket. Log derfor ind med den anden.";
-        }
-    }
+  }
 
 }
 
