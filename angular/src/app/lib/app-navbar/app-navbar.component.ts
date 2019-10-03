5a5513fcef04e2f7fa41b51b9c448af1ffc15e9b
omki <aagaardmikkeli@gmail.com>
2019-10-03 21:41:22 +0200

    grid to flex

--- angular/src/app/lib/app-navbar/app-navbar.component.ts
+++ angular/src/app/lib/app-navbar/app-navbar.component.ts
@@ -25,7 +25,7 @@ export class AppNavbarComponent implements OnInit {
     {'displayName' : "events", 'link' : "/events", 'description': "Listen over bruger arrangementer. Find dit næste event!", "selected": false},
     // {'displayName' : "min profil", 'link' : "/my-profile", 'description': "Opdater din profil og se status"},
     {'displayName' : "betaling", 'link' : "/payment", 'description': "Til betalingsgateway hvor du kan styre dit abonnement", "selected": false},
-    {'displayName' : "Min oversigt", 'link' : "/loggedin-dashboard", 'description': "Se hvad der bevæger sig og få et overblik", "selected": false},
+    // {'displayName' : "Min oversigt", 'link' : "/loggedin-dashboard", 'description': "Se hvad der bevæger sig og få et overblik", "selected": false},
   ];
 
   public loginForm = new FormGroup({
