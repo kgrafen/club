280d84bb80e067bfa60169ed677951fc168ef486
omki <aagaardmikkeli@gmail.com>
2019-10-24 17:12:47 +0200

    added seperate database

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDLgUoXPkwOuCX6aWhjUr7PXpZwox56f10",
-    authDomain: "holopedia-f204a.firebaseapp.com",
-    databaseURL: "https://holopedia-f204a.firebaseio.com",
-    projectId: "holopedia-f204a",
-    storageBucket: "holopedia-f204a.appspot.com",
-    messagingSenderId: "345316777316",
-    appId: "1:345316777316:web:235fec57ece333260fee71",
-    measurementId: "G-JS8JHERYGN"
+    apiKey: "AIzaSyDe9LCLyKorc5Mqv_Tz9ep_4DcU2xyAs5Y",
+    authDomain: "kgrafenclub.firebaseapp.com",
+    databaseURL: "https://kgrafenclub.firebaseio.com",
+    projectId: "kgrafenclub",
+    storageBucket: "kgrafenclub.appspot.com",
+    messagingSenderId: "316163411774",
+    appId: "1:316163411774:web:34d9a1cedc9b59656ca66f",
+    measurementId: "G-70RZC49BJ6"
   }
 };
 
